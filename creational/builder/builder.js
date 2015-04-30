92863d3b4e9ef720ff51c08b91e7f458ca0426c2
Maarten Berkenbosch <maarten@beebase.nl>
2015-04-30 22:26:16 +0200

    builder-spec: completed spec

--- creational/builder/builder.js
+++ creational/builder/builder.js
@@ -1,87 +1,53 @@
-(function() {
-  'use strict';
-  // director
-  function Shop() {
-    this.construct = function(builder) {
-      builder.step1();
-      builder.step2();
-      return builder.get();
-    };
+function Shop() {
+  this.construct = function(builder) {
+    builder.step1();
+    builder.step2();
+    return builder.get();
   }
+}
 
-  // builder
-  function CarBuilder() {
-    this.car = null;
-    this.step1 = function() {
-      this.car = new Car();
-    };
-    this.step2 = function() {
-      this.car.addParts();
-    };
-    this.get = function() {
-      return this.car;
-    };
+function CarBuilder() {
+  this.car = null;
+  this.step1 = function() {
+    this.car = new Car();
   }
-
-  // builder
-  function TruckBuilder() {
-    this.truck = null;
-    this.step1 = function() {
-      this.truck = new Truck();
-    };
-    this.step2 = function() {
-      this.truck.addParts();
-    };
-    this.get = function() {
-      return this.truck;
-    };
+  this.step2 = function() {
+    this.car.addParts();
   }
+  this.get = function() {
+    return this.car;
+  }
+}
 
-  //products
-  function Car() {
-    this.doors = 0;
-    this.addParts = function() {
-      this.doors = 4;
-    };
+function Car() {
+  this.doors = 0;
+  this.addParts = function() {
+    this.doors = 4;
     this.say = function() {
-      log.add('I am a ' + this.doors + '-door car.');
-    };
+      return 'I am a ' + this.doors + '-door car.';
+    }
+  }
+}
+
+function TruckBuilder() {
+  this.truck = null;
+  this.step1 = function() {
+    this.truck = new Truck();
+  }
+  this.step2 = function() {
+    this.truck.addParts();
+  }
+  this.get = function() {
+    return this.truck;
+  }
+}
+function Truck () {
+  this.doors =0;
+  this.addParts = function() {
+    this.doors = 2;
+  }
+  this.say = function(){
+    return 'I am a ' + this.doors + '-door truck.';
   }
 
-  function Truck() {
-    this.doors = 0;
-    this.addParts = function() {
-      this.doors = 2;
-    };
-    this.say = function() {
-      log.add('I am a ' + this.doors + '-door truck');
-    };
-  }
-
-  var log = (function() {
-    var result = '';
-    return {
-      add : function(msg) {
-        result += msg + '\n';
-      },
-      show: function() {
-        console.log(result);
-        result = '';
-      }
-    };
-  })();
-
-  function run() {
-    var shop = new Shop();
-    var carBuilder = new CarBuilder();
-    var truckBuilder = new TruckBuilder();
-    var car = shop.construct(carBuilder);
-    var truck = shop.construct(truckBuilder);
-    car.say();
-    truck.say();
-    log.show();
-  }
-
-  run();
-
-})();
+}
