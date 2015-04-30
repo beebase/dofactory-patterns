9803f46a41269b43deee20b77af7c4e8a1adbeab
Maarten Berkenbosch <maarten@beebase.nl>
2015-04-30 11:00:16 +0200

    builder: add spec.js

---
+++ creational/builder/builder-spec.js
@@ -0,0 +1,18 @@
+describe('Builder Pattern', function() {
+  var shop;
+  var carBuilder;
+  var truckBuilder;
+  var car;
+  var truck;
+  beforeEach(function() {
+    shop = new Shop();
+    carBuilder = new CarBuilder();
+    truckBuilder = new TruckBuilder();
+    car = shop.construct(carBuilder);
+    truck = shop.construct(truckBuilder);
+  });
+
+  it('car.say()', function() {
+    expect(car.say()).toBe('dfdf');
+  });
+});
---
+++ creational/builder/builder.js
@@ -0,0 +1,87 @@
+(function() {
+  'use strict';
+  // director
+  function Shop() {
+    this.construct = function(builder) {
+      builder.step1();
+      builder.step2();
+      return builder.get();
+    };
+  }
+
+  // builder
+  function CarBuilder() {
+    this.car = null;
+    this.step1 = function() {
+      this.car = new Car();
+    };
+    this.step2 = function() {
+      this.car.addParts();
+    };
+    this.get = function() {
+      return this.car;
+    };
+  }
+
+  // builder
+  function TruckBuilder() {
+    this.truck = null;
+    this.step1 = function() {
+      this.truck = new Truck();
+    };
+    this.step2 = function() {
+      this.truck.addParts();
+    };
+    this.get = function() {
+      return this.truck;
+    };
+  }
+
+  //products
+  function Car() {
+    this.doors = 0;
+    this.addParts = function() {
+      this.doors = 4;
+    };
+    this.say = function() {
+      log.add('I am a ' + this.doors + '-door car.');
+    };
+  }
+
+  function Truck() {
+    this.doors = 0;
+    this.addParts = function() {
+      this.doors = 2;
+    };
+    this.say = function() {
+      log.add('I am a ' + this.doors + '-door truck');
+    };
+  }
+
+  var log = (function() {
+    var result = '';
+    return {
+      add : function(msg) {
+        result += msg + '\n';
+      },
+      show: function() {
+        console.log(result);
+        result = '';
+      }
+    };
+  })();
+
+  function run() {
+    var shop = new Shop();
+    var carBuilder = new CarBuilder();
+    var truckBuilder = new TruckBuilder();
+    var car = shop.construct(carBuilder);
+    var truck = shop.construct(truckBuilder);
+    car.say();
+    truck.say();
+    log.show();
+  }
+
+  run();
+
+})();
