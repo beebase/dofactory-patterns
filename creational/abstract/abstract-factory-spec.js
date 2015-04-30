ae6cc66effcf10a8b01a1160202026136a5af60a
Maarten Berkenbosch <maarten@beebase.nl>
2015-04-30 21:00:01 +0200

    abstract-factory: add Vendor testing

--- creational/abstract/abstract-factory-spec.js
+++ creational/abstract/abstract-factory-spec.js
@@ -8,13 +8,46 @@ describe('EmployeeFactory', function() {
   it('EmployeeFactory should be a function', function() {
     expect(typeof EmployeeFactory).toBe('function');
   });
+  it('Employee should be defined', function() {
+    expect(Employee).toBeDefined();
+  });
   it('employeeFactory should be defined', function() {
     expect(employeeFactory).toBeDefined();
   });
-  it('employee.name should be Maarten', function() {
+  it('employeeFactory.create()', function() {
+    expect(employeeFactory.create).toBeDefined();
+  });
+  it('employee.name()', function() {
     expect(employee.name).toBe('Maarten');
   });
   it('employee.say()', function() {
     expect(employee.say()).toEqual('I am employee Maarten');
+  });
+});
+
+describe('VendorFactory', function() {
+  var vendorFactory;
+  var vendor;
+  beforeEach(function() {
+    vendorFactory = new VendorFactory();
+    vendor = vendorFactory.create('Maarten');
+  });
+  it('VendorFactory should be a function', function() {
+    expect(typeof VendorFactory).toBe('function');
+  });
+  it('Vendor should be defined', function() {
+    expect(Vendor).toBeDefined();
+  });
+  it('vendorFactory should be defined', function() {
+    expect(vendorFactory).toBeDefined();
+  });
+  it('vendorFactory.create()', function() {
+    expect(vendorFactory.create).toBeDefined();
+  });
+  it('vendor.name()', function() {
+    expect(vendor.name).toBe('Maarten');
+  });
+  it('vendor.say()', function() {
+    expect(vendor.say()).toEqual('I am vendor Maarten');
   });
 });
