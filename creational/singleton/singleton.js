f0dc2d6b688765f21a002c923da8cd4747d9966f
Maarten Berkenbosch <maarten@beebase.nl>
2015-05-01 11:25:12 +0200

    no message

---
+++ creational/singleton/singleton.js
@@ -0,0 +1,24 @@
+var Singleton = (function() {
+  var instance;
+
+  function createInstance() {
+    return new Object('I am the instance');
+  }
+
+  return {
+    getInstance: function() {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    }
+  };
+
+})();
+
+function run() {
+  var instance1 = Singleton.getInstance();
+  var instance2 = Singleton.getInstance();
+  console.log('Same instance? ' + (instance1 === instance2));
+}
+run();
